4305fb3cf52a7f19a1742e04bf38c026d86d7d50
Snigdhajyoti Ghosh <snigdhajyoti.ghosh@gmail.com>
2021-10-02 21:25:39 +0530

    Refactor game & move input processing to main class

--- src/main/java/com/joe/businesshouse/Main.java
+++ src/main/java/com/joe/businesshouse/Main.java
@@ -1,12 +1,61 @@
 package com.joe.businesshouse;
 
-import com.joe.businesshouse.game.GameSetup;
+import com.joe.businesshouse.bank.Bank;
+import com.joe.businesshouse.cell.Cell;
+import com.joe.businesshouse.cell.Empty;
+import com.joe.businesshouse.cell.Hotel;
+import com.joe.businesshouse.cell.Jail;
+import com.joe.businesshouse.cell.Lottery;
+import com.joe.businesshouse.game.Game;
+import com.joe.businesshouse.game.User;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        GameSetup gameSetup = new GameSetup(3, "J,H,L,H,E,L,H,L,H,J",
-                "2,2,1, 4,4,2, 4,4,2, 2,2,1, 4,4,2, 4,4,2, 2,2,1");
-        gameSetup.startGame();
-        System.out.printf("Winner is: %s%n", gameSetup.getWinner());
+        Bank bank = new Bank(5000);
+        Game game = new Game(bank,
+                getListOfDiceInput("2,2,1, 4,4,2, 4,4,2, 2,2,1, 4,4,2, 4,4,2, 2,2,1"),
+                createAndRegisterUser(3, bank),
+                initiateBoard("J,H,L,H,E,L,H,L,H,J"));
+        game.startGame();
+        System.out.printf("Winner is: %s%n", game.getWinner());
+    }
+
+    private static List<Integer> getListOfDiceInput(String diceDiagram) {
+        return Arrays.stream(diceDiagram.split(","))
+                .map(e -> Integer.parseInt(e.strip()))
+                .collect(Collectors.toList());
+    }
+
+    private static List<Cell> initiateBoard(String cellDiagram) {
+        AtomicInteger i = new AtomicInteger(1);
+
+        Map<String, Supplier<Cell>> cellMap = new HashMap<>();
+        cellMap.put("J", () -> new Jail(i.getAndIncrement()));
+        cellMap.put("L", () -> new Lottery(i.getAndIncrement()));
+        cellMap.put("H", () -> new Hotel(i.getAndIncrement()));
+        cellMap.put("E", () -> new Empty(i.getAndIncrement()));
+
+        return Arrays.stream(cellDiagram.split(","))
+                .map(e -> cellMap.get(e.strip()).get())
+                .collect(Collectors.toList());
+    }
+
+    private static List<User> createAndRegisterUser(int noOfPlayer, Bank bank) {
+        return IntStream.range(1, noOfPlayer + 1)
+                .mapToObj(e -> {
+                    User user = new User(e);
+                    bank.registerUser(user, 1000);
+                    return user;
+                })
+                .collect(Collectors.toList());
     }
 }
---
+++ src/main/java/com/joe/businesshouse/game/Game.java
@@ -0,0 +1,46 @@
+package com.joe.businesshouse.game;
+
+import com.joe.businesshouse.bank.Bank;
+import com.joe.businesshouse.cell.Cell;
+import com.joe.businesshouse.visitor.BankCellVisitor;
+import com.joe.businesshouse.visitor.CellVisitor;
+
+import java.util.List;
+
+public class Game {
+    private final Bank bank;
+    private final List<Integer> dices;
+    private final List<User> users;
+    private final List<Cell> board;
+    private int currentPayer;
+
+    public Game(Bank bank, List<Integer> dices, List<User> users, List<Cell> board) {
+        this.bank = bank;
+        this.dices = dices;
+        this.users = users;
+        this.board = board;
+        this.currentPayer = 0;
+    }
+
+    public void startGame() {
+        CellVisitor cellVisitor = new BankCellVisitor(bank);
+        System.out.println(bank);
+        for (int each : dices) {
+            User user = users.get(currentPayer);
+
+            int boardIndex = (user.getBoardIdx() + each) % board.size();
+            Cell cell = board.get(boardIndex);
+            cell.accept(user, cellVisitor);
+            user.setBoardIdx(boardIndex);
+
+            System.out.printf("%s visited by %s\n", cell, user);
+            System.out.println(bank);
+
+            currentPayer = (currentPayer + 1) % users.size();
+        }
+    }
+
+    public User getWinner() {
+        return bank.richestPlayer();
+    }
+}
